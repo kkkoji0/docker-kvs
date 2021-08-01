d5028e340471b03d79ca6cb51af8159b14477696
kkkoji0 <g10flllngmlll@gmail.com>
2021-08-01 15:40:26 +0900

    2nd commit

--- sample_code/chap4/c4flask1/server.py
+++ sample_code/chap4/c4flask1/server.py
@@ -1,7 +1,12 @@
-import os, flask
+import os
+import flask
 PORT = int(os.environ['PORT'])
 app = flask.Flask('app server')
-@app.route('/api/v1/hello')
+
+
+@app.route('/')
 def index():
-  return 'hello Dockerfile'
+    return 'hello Dockerfile'
+
+
 app.run(debug=False, host='0.0.0.0', port=PORT)
